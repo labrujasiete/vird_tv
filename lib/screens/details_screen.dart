9a61fca30daafd47114ac647d3220ae562cdc287
laBruja-Team <labrujasiete@gmail.com>
2023-02-19 23:02:25 -0800

    02-19-23 mm-dd-yy

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,12 +5,48 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final String movie =  ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      body: Container(
-        child: const Center(
-          child: Text('Details Screen'),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar(),
+          
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.transparent,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black45,
+          child: const Text(
+            'movie.title',
+            style: TextStyle( fontSize: 16 ),
+          ),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'), 
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
         ),
       ),
+
     );
   }
 }
--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -5,10 +5,9 @@ class MovieSlider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: double.infinity,
-      height: 250,
-      color: Colors.red,
+      height: 280,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -16,11 +15,12 @@ class MovieSlider extends StatelessWidget {
             padding: EdgeInsets.symmetric( horizontal: 20 ),
             child:  Text('Populares', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
           ),
+          const SizedBox(height: 10,),
           Expanded(
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
               itemCount: 10,
-              itemBuilder: ( _, int index) => const _MovieSlider(),
+              itemBuilder: ( _, int index) => const _MoviePoster(),
             ),
           ),  
         ],
@@ -29,16 +29,42 @@ class MovieSlider extends StatelessWidget {
   }
 }
 
-class _MovieSlider extends StatelessWidget {
-  const _MovieSlider({super.key});
+class _MoviePoster extends StatelessWidget {
+  const _MoviePoster({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 130,
-      height: 190,
-      color: Colors.green,
+      height: 200,
       margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                  placeholder: AssetImage('assets/no-image.jpg'), 
+                  image: NetworkImage('https://via.placeholder.com/300x400'),
+                  width: 130,
+                  height: 190,
+                  fit: BoxFit.cover,
+                ),
+            ),
+          ),
+
+            const SizedBox(height: 5,),
+
+            const Text(
+              'Star wars: el retorno del rey 4k 60 frps', 
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+              ),
+
+        ],
+      ),
     );
   }
 }
